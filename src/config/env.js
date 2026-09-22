d569aab622eac70cb4017cd53237ae619b3bd05c
Animal-gits <muhammadhadi8920@gmail.com>
2026-09-22 13:09:49 +0500

    created helpers like ApiError and ApiResponse class and AsyncHandler

--- src/config/env.js
+++ src/config/env.js
@@ -16,10 +16,24 @@ if(!process.env.PORT){
     )
     process.exit(1)
 }
+if(!process.env.ACCESS_TOKEN_SECRET){
+    console.error(
+        "Warning : ACCESS_TOKEN_SECRET is not set. Please set it in the .env file."
+    )
+    process.exit(1)
+}
+if(!process.env.REFRESH_TOKEN_SECRET){
+    console.error(
+        "Warning : REFRESH_TOKEN_SECRET is not set. Please set it in the .env file."
+    )
+    process.exit(1)
+}
 
 const config = {
     MONGO_URI : process.env.MONGO_URI,
-    PORT : process.env.PORT
+    PORT : process.env.PORT,
+    ACCESS_TOKEN_SECRET : process.env.ACCESS_TOKEN_SECRET,
+    REFRESH_TOKEN_SECRET : process.env.REFRESH_TOKEN_SECRET
 }
 
 export default Object.freeze(config)
