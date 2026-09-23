87507af2821332aa9202c23d34f9cef2977031a2
Animal-gits <muhammadhadi8920@gmail.com>
2026-09-23 13:26:32 +0500

    Made the API og Log in user

--- src/controllers/auth.controllers.js
+++ src/controllers/auth.controllers.js
@@ -1,9 +1,10 @@
 import { ApiError } from "../helpers/ApiError";
 import {User} from "../models/user.model.js"
 import {ApiResponse} from "../helpers/ApiResponse.js"
+import {asyncHandler} from "../helpers/asyncHandler.js"
 import {generateAccessToken , generateRefreshToken} from "../config/generateToken.js"
 
-const registerUser = async(req , res) => {
+const registerUser = asyncHandler(async(req , res) => {
     const {username , email , password} = req.body
 
     if(!username || username.trim() === ""){
@@ -61,4 +62,57 @@ const registerUser = async(req , res) => {
             )
     }
 
-}
+})
+
+const loginUser = asyncHandler(async (req, res) => {
+    const {email , password} = req.body
+
+    if(!email || email.trim() === ""){
+        throw new ApiError(401 , "Enter the email")
+    }
+    if(!password || password.trim() === ""){
+        throw new ApiError(401 , "Enter the password")
+    }
+
+    const user = await User.findOne({email})
+
+    if(!user){
+        throw new ApiError(404 , "User not found")
+    }
+
+    const isPasswordCorrect = await user.matchPassword(password)
+
+    if(!isPasswordCorrect){
+        throw new ApiError(404 , "User not found")
+    }
+
+    const accessToken = await generateAccessToken(user._id)
+    const refreshToken = await generateRefreshToken(user._id)
+
+    const loggedInUser = await User.findByIdAndUpdate(
+        user._id , 
+        {
+            $set : {
+                refreshToken : refreshToken
+            }
+        },
+        {new : true}
+    ).select("-password -refreshToken")
+
+    const cookieOptions = {
+        httpOnly : true,
+        secure : true
+    }
+
+    if(!loggedInUser){
+        throw new ApiError(404 , "Failed to log in User")
+    }else{
+        res
+            .status(200)
+            .cookie("accessToken" , accessToken , cookieOptions)
+            .cookie("refreshToken", refreshToken , cookieOptions)
+            .json(
+                new ApiResponse(200 , loggedInUser , "User logged-in successfuly")
+            )
+    }
+})
