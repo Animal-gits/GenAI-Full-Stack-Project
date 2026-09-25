5c4c1172aa9ffc7c47bbe075eb2b57b5009b5bbe
Animal-gits <muhammadhadi8920@gmail.com>
2026-09-25 20:39:51 +0500

    Made an API to get the interview report by id

--- src/controllers/interview.controller.js
+++ src/controllers/interview.controller.js
@@ -3,8 +3,10 @@ import { generateInterviewReport } from "../services/ai.service.js";
 import { InterviewReport } from "../models/interviewReport.model";
 import { ApiResponse } from "../helpers/ApiResponse";
 import { ApiError } from "../helpers/ApiError";
+import { asyncHandler } from "../helpers/asyncHandler";
+import mongoose from "mongoose";
 
-const generateInterviewReportController = async (req , res) => {
+const generateInterviewReportController = asyncHandler(async (req , res) => {
 
     const resumeFileContent = await (new pdfParse.PDFParse(Uint8Array.from(req.file.buffer))).getText()
 
@@ -45,8 +47,29 @@ const generateInterviewReportController = async (req , res) => {
         .json(
             new ApiResponse(201 , InterviewReportRes , "Interview Report generated sucessfully")
         )
-}
+}) 
+
+const getInterviewReportById = asyncHandler(async (req , res) => {
+    const {interviewId} = req.params
+
+    if(!mongoose.isValidObjectId(interviewId)){
+        throw new ApiError(404 , "Invalid interview ID")
+    }
+
+    const interviewReportFound = await InterviewReport.findOne({_id : interviewId , user : req.user._id})
+
+    if(!interviewReportFound){
+        throw new ApiError(404 , "Interview Report not found")
+    }
+
+    res
+        .status(200)
+        .json(
+            new ApiResponse(200 , InterviewReportFound , "Intervew Report fetched successfully")
+        )
+})
 
 export {
-    generateInterviewReportController
+    generateInterviewReportController,
+    getInterviewReportById
 }
